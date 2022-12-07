2a35eee288ecf9307736d972576d7ff62baf164a
Umang Kumar Singh <umg910@gmail.com>
2022-12-07 16:21:23 +0530

    FST-M1 REPO

--- Java/Activities/Activity1.java
+++ Java/Activities/Activity1.java
@@ -1,8 +1,15 @@
 package activities;
 
 public class Activity1 {
-    public static void main(String[] args) {
-        Car obj = new Car();
-        System.out.println(obj.name);
+        public static void main(String args[]) {
+            Car toyota = new Car();
+            toyota.make = 2014;
+            toyota.color = "black";
+            toyota.transmission = "manual";
+
+            toyota.displayCharacteristics();
+            toyota.accelarate();
+            toyota.brake();
+        }
     }
-}
+
